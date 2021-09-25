71430d1957a92305524e2695130c3d803c92dd2e
kowais915 <kowais915@gmail.com>
2021-09-25 08:18:34 +0500

    getting basic info

---
+++ acc.js
@@ -0,0 +1,50 @@
+const key = 'BcJi4B6Zbp1cheeDGw4w2wTWfjjgOArR';
+
+
+const theCity = async (city)=>{
+
+
+    const resource = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(resource + query);
+    const data = await response.json();
+  
+    return data[0];
+}
+
+
+//weatherinfo
+
+const weatherInfo = async (id)=>{
+
+    const res = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const quer = `${id}?apikey=${key}`;
+
+    const response = await fetch(res + quer);
+    const data = await response.json();
+
+    console.log(data);
+
+    return data[0];
+
+
+
+}
+
+
+
+
+
+
+// theCity('peshawar')
+//     .then(data=>{
+//         return weatherInfo(data.Key);
+//     }).then(data=>{
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         alert('error');
+//     })
+
+// weatherInfo(259370);
---
+++ app.js
@@ -0,0 +1,38 @@
+const formm = document.querySelector('form');
+const grabbed = document.getElementById('show');
+const temp = document.getElementById('temp');
+const day = document.getElementById('day');
+
+
+
+const update = async(city)=>{
+
+       const gettingCityInfo = await theCity(city);
+       const gettingId = await weatherInfo(gettingCityInfo.Key);
+       console.log(gettingId);
+
+       temp.innerHTML += `<p>${gettingId.Temperature.Metric.Value} &deg; C </p>`;
+       if(gettingId.IsDayTime == true){
+           day.innerText = "yes"
+       }
+       else{
+           day.innerText = "No";
+       }
+   
+}
+
+formm.addEventListener('submit', (e)=>{
+
+    e.preventDefault();
+
+    const cityName = formm.city.value.trim();
+    
+    formm.reset();
+
+    //show in browser
+grabbed.innerText += cityName;
+
+update(cityName);
+  
+
+})
